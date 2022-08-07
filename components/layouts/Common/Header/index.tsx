47be5f8e02e729e03d19c667197dcf9828ba7dcc
DreamEcho100 <maze6572198@gmail.com>
2022-08-07 14:58:55 +0200

    Workin on the main header accessibility

--- components/layouts/Common/Header/index.tsx
+++ components/layouts/Common/Header/index.tsx
@@ -75,9 +75,10 @@ const SubMenuOnSmallerScreens = ({
 			className={`mx-1 my-1 relative ${classes.navOnHoverContainer}`}
 			key={item.entry}
 		>
-			<p
+			<button
 				className='px-3 py-2 flex justify-between items-center hover:bg-zinc-700 rounded-lg cursor-pointer'
 				onClick={() => setIsSubMenuVisible((prev) => !prev)}
+				title={`show ${item.entry} dropdown on hover or focus`}
 			>
 				{item.entry} <span className='px-1' />
 				<span
@@ -89,7 +90,7 @@ const SubMenuOnSmallerScreens = ({
 						lineHeight: 0,
 					}}
 				/>
-			</p>
+			</button>
 			<nav className={isSubMenuVisible ? '' : 'hidden'}>
 				<ul className='mx-4 flex flex-col justify-between items-center min-w-[12rem]'>
 					{item.list.map((subItem) => (
@@ -113,16 +114,21 @@ const MainHeader = () => {
 	return (
 		<header className='z-10 relative bg-black text-white w-full text-lg font-bold h-16'>
 			<div className='w-full h-full max-w-[1400px] mx-auto flex justify-between items-center px-4'>
-				<div className='bg-zinc-700 px-4 py-1 rounded-lg flex justify-between items-center'>
-					<CustomNextImage
-						width={16}
-						height={16}
-						className='w-4 h-4'
-						src={mainHeaderData.logo.iconUrl}
-						alt=''
-					/>
-					<span className='px-1' />
-					<a href={mainHeaderData.logo.href}>{mainHeaderData.logo.text}</a>
+				<div className='bg-zinc-700 px-4 py-1 rounded-lg'>
+					<a
+						href={mainHeaderData.logo.href}
+						className=' flex justify-between items-center'
+					>
+						<CustomNextImage
+							width={16}
+							height={16}
+							className='w-4 h-4'
+							src={mainHeaderData.logo.iconUrl}
+							alt=''
+						/>
+						<span className='px-1' />
+						{mainHeaderData.logo.text}
+					</a>
 				</div>
 				<nav className='hidden lg:flex'>
 					<ul className='flex justify-between items-center'>
@@ -133,7 +139,7 @@ const MainHeader = () => {
 										className={`hover:bg-zinc-700 px-2 py-1 rounded-lg cursor-pointer relative ${classes.navOnHoverContainer}`}
 										key={item.entry}
 									>
-										<p className='flex justify-between items-center'>
+										<button className='flex justify-between items-center'>
 											{item.entry} <span className='px-1' />
 											<span
 												style={{
@@ -144,7 +150,7 @@ const MainHeader = () => {
 													lineHeight: 0,
 												}}
 											/>
-										</p>
+										</button>
 										<nav
 											className={`bg-zinc-800 rounded-2xl ${classes.navOnHover}`}
 										>
